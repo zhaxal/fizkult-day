ea45700fd769ba4d0ed8fa82c79a7f7cd5b20311
Duman <201605@astanait.edu.kz>
2023-04-19 12:38:18 +0600

    fix: cover

--- components/cover-block/Cover.tsx
+++ components/cover-block/Cover.tsx
@@ -12,7 +12,7 @@ const Cover = () => {
           <Box w="full" pos="relative" h="auto">
             <Image src={"/images/cover_wo_text.svg"} alt="cover" />
             <VStack pos="absolute" top={"2.26%"} bottom="59.89%" px={"53px"} w="full" align={"flex-start"}>
-              <Text color="white" fontFamily={"Cover"} fontSize={desktopFont ? "128px" : "80px"} lineHeight={desktopFont ? "138px" : "100px"} textTransform="uppercase" textOverflow={"ellipsis"} whiteSpace={"pre-line"} overflow={"hidden"}>День {'\n'}Физкультурника 2022</Text>
+              <Text color="white" fontFamily={"Cover"} fontSize={desktopFont ? "128px" : "80px"} lineHeight={desktopFont ? "138px" : "100px"} textTransform="uppercase" textOverflow={"ellipsis"} whiteSpace={"pre-line"} overflow={"hidden"}>День {'\n'}Физкультурника 2023</Text>
             </VStack>
           </Box>
         ) :
@@ -20,7 +20,7 @@ const Cover = () => {
             <Box w="full" pos="relative" h="auto">
               <Image src={"/images/cover_mob_wo_text.svg"} alt="cover" pos="relative" w="full" h="full" />
               <VStack pos="absolute" pl={"23px"} w="full" align={"flex-start"} top={"21.09%"} bottom="57.2%">
-                <Text color="white" fontFamily={"Cover"} fontSize={mobileFont ? "48px" : "38px"} lineHeight={mobileFont ? "52px" : "40px"} textTransform="uppercase" textOverflow={"ellipsis"} whiteSpace={"pre-line"} overflow={"hidden"}>День {'\n'}Физкультурника 2022</Text>
+                <Text color="white" fontFamily={"Cover"} fontSize={mobileFont ? "48px" : "38px"} lineHeight={mobileFont ? "52px" : "40px"} textTransform="uppercase" textOverflow={"ellipsis"} whiteSpace={"pre-line"} overflow={"hidden"}>День {'\n'}Физкультурника 2023</Text>
               </VStack>
             </Box>
           )
