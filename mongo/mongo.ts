c31e4d9fa7ce066b8391c91d1a799531ef300ea5
DumanBruh <dmarlambekov@gmail.com>
2023-08-05 22:34:13 +0600

    db test

--- mongo/mongo.ts
+++ mongo/mongo.ts
@@ -9,12 +9,10 @@ import { Form } from "./models/form";
 import { Record } from "./models/record";
 import { Setting } from "./models/setting";
 
-// const uri = process.env.MONGO_URL || "mongodb://0.0.0.0:27017";
-const uri = "mongodb://0.0.0.0:27017";
+const uri = process.env.MONGO_URL || "mongodb://0.0.0.0:27017";
 const client = new MongoClient(uri);
 client.connect();
-// const db = client.db(process.env.DB_NAME || "fd");
-const db = client.db("fd");
+const db = client.db(process.env.DB_NAME || "fd");
 
 export const recordsCol = db.collection<Record>("records");
 export const fitnessRecordsCol = db.collection<FitnessRecord>("records");
